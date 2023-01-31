618843e6d687bf4b526c40a100beb7e2e038e286
khj5606kh@gmail.com <khj5606kh@gmail.com>
2023-01-31 17:08:50 +0900

    no message

--- main.py
+++ main.py
@@ -120,18 +120,9 @@ bertmodel, vocab = get_pytorch_kobert_model()
 
 
 try:
-  model = torch.load('/model/7emotions_model.pt')
+  model = torch.load('./model/7emotions_model.pt')
 except:
   st.write("error : load model")
-## 경로 출력
-try:
-  st.write(os.path.abspath(__file__))
-except:
-  st.write("error1")
-try:
-  st.write(os.path.realpath(__file__))
-except:
-  st.write("error2")
 
 
 ## Title
