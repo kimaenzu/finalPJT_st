ec6315a5aaa6ebb0cc820c7bf6dd0d0509720dba
khj5606kh@gmail.com <khj5606kh@gmail.com>
2023-01-31 16:06:41 +0900

    kobert 모델 로드 테스트

--- main.py
+++ main.py
@@ -26,6 +26,39 @@ columns = ['MON','TUE','WED','THU','FRI','SAT','SUN']
 df.columns = columns 
 
 ############
+## kobert
+## GPU 설정
+device = torch.device("cuda:0")
+
+class BERTClassifier(nn.Module):
+  def __init__(self,
+              bert,
+              hidden_size = 768,
+              num_classes=7,
+              dr_rate=None,
+              params=None):
+    super(BERTClassifier, self).__init__()
+    self.bert = bert
+    self.dr_rate = dr_rate
+                 
+    self.classifier = nn.Linear(hidden_size , num_classes)
+    if dr_rate:
+      self.dropout = nn.Dropout(p=dr_rate)
+    
+  def gen_attention_mask(self, token_ids, valid_length):
+    attention_mask = torch.zeros_like(token_ids)
+    for i, v in enumerate(valid_length):
+      attention_mask[i][:v] = 1
+      return attention_mask.float()
+
+  def forward(self, token_ids, valid_length, segment_ids):
+    attention_mask = self.gen_attention_mask(token_ids, valid_length)
+        
+    _, pooler = self.bert(input_ids = token_ids, token_type_ids = segment_ids.long(), attention_mask = attention_mask.float().to(token_ids.device))
+    if self.dr_rate:
+      out = self.dropout(pooler)
+      return self.classifier(out)
+
 ## def 
 
 ## 일기를 작성받는 함수
@@ -81,6 +114,9 @@ def predict_img(emo):
     st.write(HTML('<img src="https://raw.githubusercontent.com/kimaenzu/finalPJT_st/main/image/disgust.png">'))
 
 ############
+## bertmodel의 vocabulary
+bertmodel, vocab = get_pytorch_kobert_model()
+model = torch.load('/models/7emotions_model.pt')
 
 ## Title
 st.title("감정저장소")
